931892279daef10eb743ccafa4f0715ee546c362
Peter Hencz <peter.hencz@gmail.com>
2018-07-18 06:12:53 +0200

    YOLO!!! 🖕 Changed: src/app/pad/pad.component.ts

--- src/app/pad/pad.component.ts
+++ src/app/pad/pad.component.ts
@@ -14,10 +14,9 @@ export class PadComponent implements OnInit {
   }
 
   const sampler = new Tone.Sampler({
-    "C3" : "/assets/drum_sounds/dm_closed_hh.mp3",
-    "D#3" : "path/to/Dsharp3.mp3",
-    "F#3" : "path/to/Fsharp3.mp3",
-    "A3" : "path/to/A3.mp3",
+    "C3" : "/assets/drum_sounds/dm_kick.mp3",
+    "D3" : "/assets/drum_sounds/dm_snare.mp3",
+    "F3" : "/assets/drum_sounds/dm_closed_hh.mp3",
    }, function(){
     sampler.triggerAttack("D3")
 }
