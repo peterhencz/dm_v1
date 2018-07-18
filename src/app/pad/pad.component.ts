d9973670e560bd4596310ae316b2b859a43dfd80
Peter Hencz <peter.hencz@gmail.com>
2018-07-18 06:11:13 +0200

    YOLO!!! 🤞 Changed: src/app/pad/pad.component.ts

--- src/app/pad/pad.component.ts
+++ src/app/pad/pad.component.ts
@@ -14,11 +14,11 @@ export class PadComponent implements OnInit {
   }
 
   const sampler = new Tone.Sampler({
-   *   "C3" : "/assets/drum_sounds/dm_closed_hh.mp3",
-   *   "D#3" : "path/to/Dsharp3.mp3",
-   *   "F#3" : "path/to/Fsharp3.mp3",
-   *   "A3" : "path/to/A3.mp3",
-   * }, function(){
-   *   //sampler will repitch the closest sample
-   *   sampler.triggerAttack("D3")
+    "C3" : "/assets/drum_sounds/dm_closed_hh.mp3",
+    "D#3" : "path/to/Dsharp3.mp3",
+    "F#3" : "path/to/Fsharp3.mp3",
+    "A3" : "path/to/A3.mp3",
+   }, function(){
+    sampler.triggerAttack("D3")
 }
+
