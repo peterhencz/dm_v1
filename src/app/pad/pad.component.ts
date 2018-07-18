ff6af4d8918804cfef77296013f25360eff5c48e
Peter Hencz <peter.hencz@gmail.com>
2018-07-18 06:17:44 +0200

    YOLO!!! 🤓  Changed: src/app/pad/pad.component.ts

--- src/app/pad/pad.component.ts
+++ src/app/pad/pad.component.ts
@@ -8,7 +8,20 @@ import * as Tone from 'tone';
 })
 export class PadComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+  const zongi = new Tone.Sampler({
+  "C3" : "./src/static/piano_c3.mp3",
+  "D#3" : "./src/static/Piano_d3.wav",
+  "F#3" : "./src/static/Piano_f3.wav",
+  "A3" : "./src/static/Piano_a3.wav"
+}, function() {
+  let time = '2n';
+  zongi.triggerAttack("E4", time)
+  zongi.triggerAttack("C0", time);
+  zongi.triggerAttack("B#2")
+  zongi.triggerAttack(time + 0.6);
+}).toMaster();
+}
 
   ngOnInit() {
   }
