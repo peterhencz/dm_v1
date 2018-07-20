28928e9b6ebcf33de14928f39dd6a55ba63fb539
Peter Hencz <peter.hencz@gmail.com>
2018-07-21 01:42:33 +0200

    YOLO!!! 😎 Changed: src/app/pad/pad.component.ts

--- src/app/pad/pad.component.ts
+++ src/app/pad/pad.component.ts
@@ -10,7 +10,7 @@ export class PadComponent implements OnInit {
 
   kick = new Tone.Player({
     "url" : "./assets/drum_sounds/dm_kick.mp3",
-  }).toMaster();
+  }).triggerAttack();
 
   constructor() { }
 
