c4ba6c2ab7e540544cdc20159b1c23be1693bc5a
Peter Hencz <peter.hencz@gmail.com>
2018-07-23 22:32:28 +0200

    YOLO!!! 😵 Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -14,15 +14,16 @@ export class DrumpadsComponent implements OnInit {
     "url" : "./assets/drum_sounds/dm_kick.mp3",
   }).toMaster();
 
+  seq = new Tone.Sequence(function(time, sound){
+    console.log(sound);
+  }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "8n").start(0);
+  
   constructor() { }
 
   ngOnInit() {
   }
 
   playSeq() {
-    const seq = new Tone.Sequence(function(time, sound){
-      console.log(sound);
-    }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "8n").start(0);
         console.log('cicafasza');
     Tone.Transport.start();
   }
