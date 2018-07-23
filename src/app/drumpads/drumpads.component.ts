a71c8f45ba9fd533f3ee92e339b9dc3a66a044a9
Peter Hencz <peter.hencz@gmail.com>
2018-07-23 22:25:36 +0200

    YOLO!!! 😎 Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -23,7 +23,7 @@ export class DrumpadsComponent implements OnInit {
 
   playSeq() {
     const seq = new Tone.Sequence(function(time, sound){
-      this.selectedSound.start();
+      this.selectedSound;
     }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "8n").start(0);
         console.log('cicafasza');
     Tone.Transport.start();
