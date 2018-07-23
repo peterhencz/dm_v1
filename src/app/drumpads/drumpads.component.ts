59879c859f3b79cf52ad40c33116d5026e0b2752
Peter Hencz <peter.hencz@gmail.com>
2018-07-23 22:13:42 +0200

    YOLO!!! 🤓  Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -20,7 +20,7 @@ export class DrumpadsComponent implements OnInit {
       
       console.log(note);
 
-    }, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15).start(0);
+    }, '0', '1', '2', 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15).start(0);
 
     Tone.Transport.start();
 
