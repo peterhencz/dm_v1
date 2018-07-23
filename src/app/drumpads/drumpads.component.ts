9d5b86901a69bb3d634a8d5cc15d6bb8251744b1
Peter Hencz <peter.hencz@gmail.com>
2018-07-23 22:16:35 +0200

    YOLO!!! 😵 Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -16,9 +16,8 @@ export class DrumpadsComponent implements OnInit {
   }
 
   playSeq() {
-  var seq = new Tone.Sequence(function(time, note){
+  var seq = new Tone.Sequence(function(time, sound){
   console.log(note);
-//straight quater notes
 }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n").start(0);
 
     Tone.Transport.start();
