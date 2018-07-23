a296a43b6059d7334f06111d31dae0559abc9b5b
Peter Hencz <peter.hencz@gmail.com>
2018-07-23 22:24:35 +0200

    YOLO!!! 😎 Changed: src/app/pad/pad.component.ts

--- src/app/pad/pad.component.ts
+++ src/app/pad/pad.component.ts
@@ -16,7 +16,7 @@ export class PadComponent implements OnInit {
   }
 
   playSound() {
-    this.soundService.selectedSound.start();
+    this.selectedSound.start();
     console.log(this.soundService.selectedSound);
   }
 
