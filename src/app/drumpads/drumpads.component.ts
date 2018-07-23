10a9bf533562bcd59c9e0067622168d441b770fc
Peter Hencz <peter.hencz@gmail.com>
2018-07-23 23:49:29 +0200

    YOLO!!! 🖕 Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -17,7 +17,7 @@ export class DrumpadsComponent implements OnInit {
   seq = new Tone.Sequence(function(time, sound){
     console.log(time, sound);
     if (sound = 4 ) {
-      this.kick.start();
+      console.log('cica');
     }
   }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n");
   
