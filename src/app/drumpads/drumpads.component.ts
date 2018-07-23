1548f029abdad837ff765aa661d4dc6199284480
Peter Hencz <peter.hencz@gmail.com>
2018-07-23 21:56:24 +0200

    YOLO!!! 👊 Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -19,6 +19,7 @@ export class DrumpadsComponent implements OnInit {
     const seq = new Tone.Sequence(function(time, note){
       console.log(note);
     }, ["C4", "E4", "G4", "A4"], "4n");
+    Tone.Transport.start()
     console.log('cica');
   }
 
