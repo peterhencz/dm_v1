db60a3784785377b75eeede7a47a81bf0a2ed8de
Peter Hencz <peter.hencz@gmail.com>
2018-07-23 23:51:06 +0200

    YOLO!!! 🤞 Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -18,7 +18,6 @@ export class DrumpadsComponent implements OnInit {
     console.log(time, sound);
     if (sound == 4 ) {
       console.log('cica');
-      this.kick;
     }
   }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n");
   
