bedbdc45c26ceed2df647abf6c3df018861995a5
Peter Hencz <peter.hencz@gmail.com>
2018-07-23 22:26:00 +0200

    YOLO!!! 💩 Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as Tone from 'tone';
 import { PadComponent } from '../pad/pad.component';
-import { SoundService } from '../sound.service';
+
 
 @Component({
   selector: 'app-drumpads',
@@ -10,20 +10,14 @@ import { SoundService } from '../sound.service';
 })
 export class DrumpadsComponent implements OnInit {
 
-  selectedSound = new Tone.Player({
-    "url" : "./assets/drum_sounds/dm_kick.mp3",
-  }).toMaster();
-
-  constructor(
-    public soundService: SoundService
-  ) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
   playSeq() {
     const seq = new Tone.Sequence(function(time, sound){
-      this.selectedSound;
+      console.log(sound);
     }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "8n").start(0);
         console.log('cicafasza');
     Tone.Transport.start();
