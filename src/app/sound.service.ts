e75d788b3b9d17e101d56b4c9262c60c7086aab0
Peter Hencz <peter.hencz@gmail.com>
2018-07-23 20:54:47 +0200

    YOLO!!! 🖕 Changed: src/app/sound.service.ts

--- src/app/sound.service.ts
+++ src/app/sound.service.ts
@@ -6,19 +6,6 @@ export class SoundService {
 
   selectedSound;
 
-  kick = new Tone.Player({
-    "url" : "./assets/drum_sounds/dm_kick.mp3",
-  }).toMaster();
-
-  hh = new Tone.Player({
-    "url" : "./assets/drum_sounds/dm_closed_hh.mp3",
-  }).toMaster();
-
-  snare = new Tone.Player({
-    "url" : "./assets/drum_sounds/dm_snare_clap.mp3",
-  }).toMaster();
-
-
   constructor() {
   }
 
