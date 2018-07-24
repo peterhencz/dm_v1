8b56cce3411cc351d6cdcac9534d725ec07cb1a6
Peter Hencz <peter.hencz@gmail.com>
2018-07-24 15:19:35 +0200

    YOLO!!! 😅 Changed: src/app/pads.ts

--- src/app/pads.ts
+++ src/app/pads.ts
@@ -13,5 +13,15 @@ export const PADS: Pad[] = [
    { padId: 2, status: false, flash: false },  
    { padId: 3, status: false, flash: false },  
    { padId: 4, status: false, flash: false },  
-   { padId: 5, status: false, flash: false }
+   { padId: 5, status: false, flash: false },  
+   { padId: 6, status: false, flash: false },  
+   { padId: 7, status: false, flash: false },  
+   { padId: 8, status: false, flash: false },                     
+   { padId: 9, status: false, flash: false },  
+   { padId: 10, status: false, flash: false },
+   { padId: 11, status: false, flash: false },
+   { padId: 12, status: false, flash: false },
+   { padId: 13, status: false, flash: false },
+   { padId: 14, status: false, flash: false },
+   { padId: 15, status: false, flash: false }
 ] 
