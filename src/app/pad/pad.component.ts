ae2fc0dd9786b1bfebffeee772276f4721880091
Peter Hencz <peter.hencz@gmail.com>
2018-07-24 12:54:37 +0200

    YOLO!!! ☠️ Changed: src/app/pad/pad.component.ts

--- src/app/pad/pad.component.ts
+++ src/app/pad/pad.component.ts
@@ -33,7 +33,7 @@ export class PadComponent implements OnInit {
       this.Pad.status = true
     }
     console.log(this.Pad.status);
-    console.log(this.Pad.flash);
+    console.log(this.Pad);
   }
 
 }
