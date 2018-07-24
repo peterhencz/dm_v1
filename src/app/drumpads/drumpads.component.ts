0fbc63af96037ee7e16f062fdd603b0017d49c34
Peter Hencz <peter.hencz@gmail.com>
2018-07-24 15:46:07 +0200

    YOLO!!! 😅 Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -30,10 +30,10 @@ export class DrumpadsComponent implements OnInit {
   getPads(): void {
     this.padService.getPads()
       .subscribe(pads => {
-        this.pads = PADS
+        this.pads = pads
         this.seq = new Tone.Sequence(function(time, sound){
     console.log(time, sound);
-  }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n");
+  }, pads, "16n");
 
       });
   }
