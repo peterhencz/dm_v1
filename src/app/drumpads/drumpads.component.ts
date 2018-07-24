446ba07072923bbdae1c93e0798e5fd4781fc64d
Peter Hencz <peter.hencz@gmail.com>
2018-07-24 07:14:55 +0200

    YOLO!!! 🖕 Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -18,7 +18,7 @@ export class DrumpadsComponent implements OnInit {
   }).toMaster();
 
   seq = new Tone.Sequence(function(time, sound){
-    // console.log(time, sound);
+    console.log(time, sound);
   }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n");
   
   constructor(public padService: PadService) { }
@@ -36,6 +36,5 @@ export class DrumpadsComponent implements OnInit {
   playSeq() {
     this.seq.start(0);
     Tone.Transport.start();
-    console.log(this.seq)
   }
 }
