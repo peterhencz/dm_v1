e3e9d62e1e57c94c2772ef7a2ffa05b5b87f6adc
Peter Hencz <peter.hencz@gmail.com>
2018-07-24 02:19:07 +0200

    YOLO!!! 😵 Changed: src/app/pads.ts

---
+++ src/app/pads.ts
@@ -0,0 +1,51 @@
+export class Pad {
+  pad1: [
+     { status: true}
+  ],
+  pad2: [
+     { status: true}
+  ],
+  pad3: [
+     { status: true}
+  ],
+  pad4: [
+     { status: true}
+  ],
+  pad5: [
+     { status: true}
+  ],
+  pad6: [
+     { status: true}
+  ],
+  pad7: [
+     { status: true}
+  ],
+  pad8: [
+     { status: true}
+  ],
+  pad9: [
+     { status: true}
+  ],
+  pad10: [
+     { status: true}
+  ],
+  pad11: [
+     { status: true}
+  ],
+  pad12: [
+     { status: true}
+  ],
+  pad13: [
+     { status: true}
+  ],
+  pad14: [
+     { status: true}
+  ],
+  pad15: [
+     { status: true}
+  ],
+  pad16: [
+     { status: true}
+  ],
+
+}
