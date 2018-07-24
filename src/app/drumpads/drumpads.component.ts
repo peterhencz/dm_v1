ed47a7005c6e9ddceb005483f206980b49ee31ad
Peter Hencz <peter.hencz@gmail.com>
2018-07-24 13:27:20 +0200

    YOLO!!! 👊 Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -35,7 +35,7 @@ export class DrumpadsComponent implements OnInit {
   playSeq() {
     this.seq.start(0);
     Tone.Transport.start();
-    console.log(this.pads.padId);
+    console.log(this.pads[0].padId);
   }
 
   stopSeq() {
