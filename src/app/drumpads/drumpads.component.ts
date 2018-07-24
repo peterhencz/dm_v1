9442c3364ccfb027f43f44ccf5d795f3a4760c65
Peter Hencz <peter.hencz@gmail.com>
2018-07-24 03:48:22 +0200

    YOLO!!! 😉 Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -12,7 +12,6 @@ export class DrumpadsComponent implements OnInit {
 
   PADS: Pad[];
 
-
   kick = new Tone.Player({
     "url" : "./assets/drum_sounds/dm_kick.mp3",
   }).toMaster();
@@ -22,7 +21,7 @@ export class DrumpadsComponent implements OnInit {
   }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n");
   
   constructor( 
-    public Pad: Pad
+
     ) { 
   }
 
