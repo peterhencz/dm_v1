7685b146a25a50049a9bc3b224d060964d8d037c
Peter Hencz <peter.hencz@gmail.com>
2018-07-24 03:50:08 +0200

    YOLO!!! ☠️ Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -21,7 +21,7 @@ export class DrumpadsComponent implements OnInit {
     console.log(time, sound);
   }, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n");
   
-  constructor(public Pad: Pad) { }
+  constructor() { }
 
   ngOnInit() {
     this.getPads();
