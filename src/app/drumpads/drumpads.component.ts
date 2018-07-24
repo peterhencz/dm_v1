9f92c0b2891d9c765245b94450c0af0cd2630ede
Peter Hencz <peter.hencz@gmail.com>
2018-07-24 04:01:49 +0200

    YOLO!!! ☠️ Changed: src/app/drumpads/drumpads.component.ts

--- src/app/drumpads/drumpads.component.ts
+++ src/app/drumpads/drumpads.component.ts
@@ -28,7 +28,7 @@ export class DrumpadsComponent implements OnInit {
 
   getPads(): void {
     for (let i = 0; i < 16; i++) {
-      const pad = new Pad(PADS[i].padId)
+      const pad = new Pad(i)
       console.log(PADS[i].padId);
       console.log(pad);
     }
