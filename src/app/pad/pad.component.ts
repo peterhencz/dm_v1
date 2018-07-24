5af79918a3c5a98322b4adf066bb4d9389fe3407
Peter Hencz <peter.hencz@gmail.com>
2018-07-24 15:24:47 +0200

    YOLO!!! 🙃 Changed: src/app/pad/pad.component.ts

--- src/app/pad/pad.component.ts
+++ src/app/pad/pad.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import * as Tone from 'tone';
 import { SoundService } from '../sound.service';
 import { DrumpadsComponent } from '../drumpads/drumpads.component';
